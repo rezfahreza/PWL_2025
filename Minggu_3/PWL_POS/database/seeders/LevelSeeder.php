100835ea9b0a0753a2b0b2ea91e988d33ba9b023
Fahreza <fahreza200412@gmail.com>
2025-03-07 00:29:30 +0700

    pembetulan prak 6

--- Minggu_3/PWL_POS/database/seeders/LevelSeeder.php
+++ Minggu_3/PWL_POS/database/seeders/LevelSeeder.php
@@ -17,6 +17,7 @@ class Levelseeder extends Seeder
             ['level_id' => 1, 'level_kode' => 'ADM', 'level_nama' => 'Administrator'],
             ['level_id' => 2, 'level_kode' => 'MNG', 'level_nama' => 'Manager'],
             ['level_id' => 3, 'level_kode' => 'STF', 'level_nama' => 'Staff/Kasir'],
+            ['level_id' => 5, 'level_kode' => 'SPM', 'level_nama' => 'Satpam']
         ];
         DB::table('m_level')->insert($data);        
     }
