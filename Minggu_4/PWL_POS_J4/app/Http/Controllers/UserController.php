c012657b21daa1831f8200065f9ef00c996c7327
Fahreza <fahreza200412@gmail.com>
2025-03-13 02:23:01 +0700

    praktikum2.5_1_j4

--- Minggu_4/PWL_POS_J4/app/Http/Controllers/UserController.php
+++ Minggu_4/PWL_POS_J4/app/Http/Controllers/UserController.php
@@ -10,17 +10,29 @@ class UserController extends Controller
 {
     public function index()
     {
-
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2,
-            ]
-        );
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+        
+        $user->username = 'manager56';
+        
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // false
+        $user->isDirty(['nama', 'username']); // true
+        
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // true
+        $user->isClean(['nama', 'username']); // false
+        
         $user->save();
         
-        return view('user', ['data' => $user]);
+        $user->isDirty(); // false
+        $user->isClean(); // true
+        dd($user->isDirty());
     }
 }
