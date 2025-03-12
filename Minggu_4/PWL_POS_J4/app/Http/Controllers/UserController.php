c2e814d7fdd3deadfcef4fbe12ad51c36fd6ed9e
Fahreza <fahreza200412@gmail.com>
2025-03-12 13:40:20 +0700

    praktikum2_3_j4

--- Minggu_4/PWL_POS_J4/app/Http/Controllers/UserController.php
+++ Minggu_4/PWL_POS_J4/app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
     public function index()
     {
 
-        $user = UserModel::where('level_id', 1)->first();
+        $user = UserModel::firstWhere('level_id', 1);
         return view('user', ['data' => $user]);
     }
 }
