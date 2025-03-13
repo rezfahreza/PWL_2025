66440601b329276da95fb982586ad0d53bfa7516
Fahreza <fahreza200412@gmail.com>
2025-03-13 22:57:29 +0700

    praktikum2.6_3_j4

--- Minggu_4/PWL_POS_J4/routes/web.php
+++ Minggu_4/PWL_POS_J4/routes/web.php
@@ -13,3 +13,4 @@ Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
 Route::get('/user/tambah', [UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
