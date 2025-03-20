df785d30bbf8852ddc0b6316d2f2ba72922436cf
Fahreza <fahreza200412@gmail.com>
2025-03-20 21:40:38 +0700

    praktikum2_j5

--- Minggu_5/PWL_POS_J5/resources/views/layouts/app.blade.php
+++ Minggu_5/PWL_POS_J5/resources/views/layouts/app.blade.php
@@ -1,17 +1,19 @@
-@extends('adminlte::page') 
- 
+@extends('adminlte::page')
+
 {{-- Extend and customize the browser title --}} 
 @section('title') 
     {{ config('adminlte.title') }} 
     @hasSection('subtitle') | @yield('subtitle') @endif 
-@stop 
- 
+@stop
+
+@vite('resources/js/app.js')
+
 {{-- Extend and customize the page content header --}} 
 @section('content_header') 
     @hasSection('content_header_title') 
         <h1 class="text-muted"> 
             @yield('content_header_title') 
-            @hasSection('content_header_subtitle') 
+            @hasSection('content_header_subtitle')
                 <small class="text-dark"> 
                     <i class="fas fa-xs fa-angle-right text-muted"></i> 
                     @yield('content_header_subtitle') 
@@ -20,44 +22,54 @@
         </h1> 
     @endif 
 @stop 
- 
-{{-- Rename section content to content_body --}}  
+
+{{-- Rename section content to content_body --}} 
 @section('content') 
     @yield('content_body') 
 @stop 
- 
-{{-- Create a common footer --}}  
+
+
+{{-- Create a common footer --}} 
 @section('footer') 
     <div class="float-right"> 
-        Version: {{ config('app.version', '1.0.0') }} 
+        Version: {{ config('app.version', '1.0.0') }}
     </div> 
-    <strong>
+
+    <strong> 
         <a href="{{ config('app.company_url', '#') }}"> 
             {{ config('app.company_name', 'My company') }} 
         </a> 
     </strong> 
 @stop 
- 
-{{-- Add common Javascript/Jquery code --}}  
+
+
+{{-- Add common Javascript/Jquery code --}} 
+
+
+
 @push('js') 
-<script> 
-    $(document).ready(function() { 
-        // Add your common script logic here... 
-    }); 
-</script> 
+<script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script> 
+
 @endpush 
- 
+
+@stack('scripts') 
+
+
 {{-- Add common CSS customizations --}} 
-@push('css') 
+
+@push('css')
+<link 
+rel="stylesheet" 
+href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css" /> 
 <style type="text/css"> 
-    {{-- You can add AdminLTE customizations here --}} 
-    /* 
-    .card-header { 
-        border-bottom: none; 
-    } 
-    .card-title { 
-        font-weight: 600; 
-    } 
-    */ 
+{{-- You can add AdminLTE customizations here --}} 
+/* 
+.card-header { 
+border-bottom: none; 
+} 
+.card-title { 
+font-weight: 600; 
+} 
+*/ 
 </style> 
-@endpush
+@endpush 
