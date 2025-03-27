4e7fc24d7f21270a86a48944c5dcfc668f29c080
Fahreza <fahreza200412@gmail.com>
2025-03-27 22:30:53 +0700

    praktikum4_J5 Baru

--- Minggu_5_Baru/PWL_POS_J5/app/Http/Controllers/UserController.php
+++ Minggu_5_Baru/PWL_POS_J5/app/Http/Controllers/UserController.php
@@ -24,9 +24,12 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // Menentukan menu yang aktif
 
+        $level = LevelModel::all(); //Ambil data level untuk filter level
+
         return view('user.index', [
             'breadcrumb' => $breadcrumb,
             'page' => $page,
+            'level' => $level,
             'activeMenu' => $activeMenu
         ]);
     }
@@ -35,6 +38,11 @@ class UserController extends Controller
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')->with('level');
+
+        // Filter data user berdasarkan level_id
+        if (!empty($request->level_id)) {
+            $users->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             // Menambahkan kolom index / nomor urut (default nama kolom: DT_RowIndex)
