ea271e0701f667a763fd8b2957d7ef86ab36c53c
Fahreza <fahreza200412@gmail.com>
2025-04-10 14:41:27 +0700

    perbaikan praktikum1_J7

--- Minggu_7/PWL_POS_J7/app/Models/UserModel.php
+++ Minggu_7/PWL_POS_J7/app/Models/UserModel.php
@@ -5,8 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
--- Minggu_7/PWL_POS_J7/resources/views/auth/login.blade.php
+++ Minggu_7/PWL_POS_J7/resources/views/auth/login.blade.php
@@ -6,15 +6,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Login Pengguna</title>
   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+ <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+ <!-- Font Awesome -->
+ <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+ <!-- icheck bootstrap -->
+ <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
   <!-- SweetAlert2 -->
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap4.min.css') }}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+ <link rel="stylesheet" href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
+ <!-- Theme style -->
+ <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
 </head>
 
 <body class="hold-transition login-page">
@@ -62,16 +62,16 @@
   </div>
 
   <!-- jQuery -->
-  <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
-  <!-- Bootstrap 4 -->
-  <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-  <!-- jquery-validation -->
-  <script src="{{ asset('adminlte/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-  <script src="{{ asset('adminlte/plugins/jquery-validation/additional-methods.min.js') }}"></script>
-  <!-- SweetAlert2 -->
-  <script src="{{ asset('adminlte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
-  <!-- AdminLTE App -->
-  <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
+ <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+ <!-- Bootstrap 4 -->
+ <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+ <!-- jquery-validation -->
+ <script src="{{ asset('adminlte/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
+ <script src="{{ asset('adminlte/plugins/jquery-validation/additional-methods.min.js') }}"></script>
+ <!-- SweetAlert2 -->
+ <script src="{{ asset('adminlte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
+ <!-- AdminLTE App -->
+ <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
   <script>
     $.ajaxSetup({
       headers: {
@@ -83,7 +83,7 @@
       $("#form-login").validate({
         rules: {
           username: { required: true, minlength: 4, maxlength: 20 },
-          password: { required: true, minlength: 6, maxlength: 20 }
+          password: { required: true, minlength: 5, maxlength: 20 }
         },
         submitHandler: function (form) {
           $.ajax({
