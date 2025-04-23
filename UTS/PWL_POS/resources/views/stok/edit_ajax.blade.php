7b9c0a6cb0be6c813e1b42b52cdb0a674fbde918
Fahreza <fahreza200412@gmail.com>
2025-04-24 00:52:39 +0700

    perbaikan edit stok

--- UTS/PWL_POS/resources/views/stok/edit_ajax.blade.php
+++ UTS/PWL_POS/resources/views/stok/edit_ajax.blade.php
@@ -17,60 +17,73 @@
     </div>
 </div>
 @else
-<form action="{{ url('/stok/' . $stok->stok_id.'/update_ajax') }}" method="POST" id="form-edit">
+<form action="{{ url('/stok/' . $stok->stok_id . '/update_ajax') }}" method="POST" id="form-edit">
     @csrf
     @method('PUT')
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit Data Stok</h5>
+                <h5 class="modal-title">Edit Data Stok</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
+
             <div class="modal-body">
-                <div class="form-group"> 
-                    <label>Nama Barang</label> 
-                    <select name="barang_id" id="barang_id" class="form-control" required> 
-                        <option value="">- Pilih Nama Barang -</option> 
-                        @foreach($barang as $b) 
-                            <option value="{{ $b->barang_id }}">{{ $b->barang_nama }}</option> 
-                        @endforeach 
-                    </select> 
-                    <small id="error-level_id" class="error-text form-text text-danger"></small> 
-                </div> 
-                <div class="form-group"> 
-                    <label>Nama Supplier</label> 
-                    <select name="supplier_id" id="supplier_id" class="form-control" required> 
-                        <option value="">- Pilih Nama Supplier -</option> 
-                        @foreach($supplier as $s) 
-                            <option value="{{ $s->supplier_id }}">{{ $s->supplier_nama }}</option> 
-                        @endforeach 
-                    </select> 
-                    <small id="error-level_id" class="error-text form-text text-danger"></small> 
-                </div> 
-                <div class="form-group"> 
-                    <label>User</label> 
-                    <select name="user_id" id="user_id" class="form-control" required> 
-                        <option value="">- Pilih User -</option> 
-                        @foreach($user as $u) 
-                            <option value="{{ $u->user_id }}">{{ $u->username }}</option> 
-                        @endforeach 
-                    </select> 
-                    <small id="error-level_id" class="error-text form-text text-danger"></small> 
-                </div> 
-                <div class="form-group"> 
-                    <label>Tanggal</label> 
-                    <input value="" type="text" name="stok_tanggal" id="stok_tanggal" class="form-control" required> 
-                    <small id="error-stok_tanggal" class="error-text form-text text-danger"></small> 
-                </div> 
-                <div class="form-group"> 
-                    <label>Jumlah</label> 
-                    <input value="" type="text" name="stok_jumlah" id="stok_tanggal" class="form-control" required> 
-                    <small id="error-stok_jumlah" class="error-text form-text text-danger"></small> 
+                <div class="form-group">
+                    <label>Nama Barang</label>
+                    <select name="barang_id" id="barang_id" class="form-control" required>
+                        <option value="">- Pilih Nama Barang -</option>
+                        @foreach($barang as $b)
+                            <option value="{{ $b->barang_id }}" {{ $stok->barang_id == $b->barang_id ? 'selected' : '' }}>
+                                {{ $b->barang_nama }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <small id="error-barang_id" class="error-text form-text text-danger"></small>
                 </div>
+
+                <div class="form-group">
+                    <label>Nama Supplier</label>
+                    <select name="supplier_id" id="supplier_id" class="form-control" required>
+                        <option value="">- Pilih Nama Supplier -</option>
+                        @foreach($supplier as $s)
+                            <option value="{{ $s->supplier_id }}" {{ $stok->supplier_id == $s->supplier_id ? 'selected' : '' }}>
+                                {{ $s->supplier_nama }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <small id="error-supplier_id" class="error-text form-text text-danger"></small>
+                </div>
+
+                <div class="form-group">
+                    <label>User</label>
+                    <select name="user_id" id="user_id" class="form-control" required>
+                        <option value="">- Pilih User -</option>
+                        @foreach($user as $u)
+                            <option value="{{ $u->user_id }}" {{ $stok->user_id == $u->user_id ? 'selected' : '' }}>
+                                {{ $u->username }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <small id="error-user_id" class="error-text form-text text-danger"></small>
+                </div>
+
+                <div class="form-group"> 
+                    <label>Tanggal Masuk</label> 
+                    <input type="datetime-local" name="stok_tanggal" id="stok_tanggal" class="form-control" required
+                        value="{{ old('stok_tanggal', date('Y-m-d\TH:i', strtotime($stok->stok_tanggal))) }}"> 
+                    <small id="error-stok_tanggal" class="error-text form-text text-danger"></small> 
+                </div>
+                
+
+                <div class="form-group">
+                    <label>Jumlah</label>
+                    <input value="{{ $stok->stok_jumlah }}" type="text" name="stok_jumlah" id="stok_jumlah" class="form-control" required>
+                    <small id="error-stok_jumlah" class="error-text form-text text-danger"></small>
                 </div>
             </div>
+
             <div class="modal-footer">
                 <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
                 <button type="submit" class="btn btn-primary">Simpan</button>
@@ -78,31 +91,32 @@
         </div>
     </div>
 </form>
+
 <script>
     $(document).ready(function() {
         $("#form-edit").validate({
             rules: {
                 barang_id: {
-                        required: true,
-                        number: true
-                    },
-                    user_id: {
-                        required: true,
-                        number: true
-                    },
-                    supplier_id: {
-                        required: true,
-                        number: true
-                    },
-                    stok_tanggal: {
-                        required: true,
-                        date: true
-                    },
-                    stok_jumlah: {
-                        required: true,
-                        number: true,
-                        min: 1
-                    }
+                    required: true,
+                    number: true
+                },
+                supplier_id: {
+                    required: true,
+                    number: true
+                },
+                user_id: {
+                    required: true,
+                    number: true
+                },
+                stok_tanggal: {
+                    required: true,
+                    date: true
+                },
+                stok_jumlah: {
+                    required: true,
+                    number: true,
+                    min: 1
+                }
             },
             submitHandler: function(form) {
                 $.ajax({
@@ -110,7 +124,7 @@
                     type: form.method,
                     data: $(form).serialize(),
                     success: function(response) {
-                        if(response.status) {
+                        if (response.status) {
                             $('#myModal').modal('hide');
                             Swal.fire({
                                 icon: 'success',
