623559441a9b904ec08349692bcd63aa0509a7f7
Fahreza <fahreza200412@gmail.com>
2025-05-01 00:14:45 +0700

    JS10_Praktikum3

--- Minggu_10/PWL_POS_J10/routes/api.php
+++ Minggu_10/PWL_POS_J10/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::post('/login', LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', LogoutController::class)->name('logout');
