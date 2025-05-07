4b6a5a58a747a700240e3b1bbd4dc62259ab84b6
Fahreza <fahreza200412@gmail.com>
2025-05-07 17:12:48 +0700

    Tugas_JS11

--- Minggu_11/PWL_POS_J11/app/Http/Controllers/Api/BarangController.php
+++ Minggu_11/PWL_POS_J11/app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\BarangModel;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -13,8 +14,33 @@ class BarangController extends Controller
     }
 
     public function store(request $request){
-        $barang = BarangModel::create($request->all());
-        return response()->json($barang,201);
+        $validator = Validator::make($request->all(), [
+            'kategori_id' => ['required', 'integer'],
+            'barang_kode' => ['required', 'string', 'min:3', 'max:100', 'unique:m_barang,barang_kode'],
+            'barang_nama' => ['required', 'string', 'min:3', 'max:100'],
+            'harga_beli' => ['required', 'numeric', 'min:0'],
+            'harga_jual' => ['required', 'numeric', 'min:0'],
+            'image' => ['required', 'image', 'mimes:jpeg,png,jpeg,gif', 'max:2048']
+        ]);
+        
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+        
+        $image = $request->file('image');
+        
+        $barang = BarangModel::create([
+            'kategori_id' => $request->kategori_id,
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'image' => $image->hashName(),
+        ]);
+        
+        return response()->json($barang, 201);        
+        //$barang = BarangModel::create($request->all());
+        //return response()->json($barang,201);
     }
 
     public function show(BarangModel $barang)
